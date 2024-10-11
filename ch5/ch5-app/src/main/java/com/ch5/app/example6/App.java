61dedc5f8d811c9840b1fac5991bb3b6a1ef4e71
SerenaPang4 <ypang4@usfca.edu>
2024-10-10 18:17:12 -0700

    using prototype bean

---
+++ ch5/ch5-app/src/main/java/com/ch5/app/example6/App.java
@@ -0,0 +1,19 @@
+package com.ch5.app.example6;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.ch5.app.CommentService;
+
+/**
+ */
+public class App {
+	public static void main(String[] args) {
+		var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+		var cs1 = c.getBean("commentService", CommentService.class);
+		var cs2 = c.getBean("commentService", CommentService.class);
+		//The variable cs1 and cs2 always contain reference to two different instances
+
+		boolean b1 = cs1 == cs2;
+		System.out.println(b1);
+	}
+}
---
+++ ch5/ch5-app/src/main/java/com/ch5/app/example6/CommentRepository.java
@@ -0,0 +1,15 @@
+package com.ch5.app.example6;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentRepository {
+	@Autowired
+	private CommentRepository commentRepository;
+	
+	public CommentRepository getCommentRepository() {
+		return commentRepository;
+	}
+}
+ 
