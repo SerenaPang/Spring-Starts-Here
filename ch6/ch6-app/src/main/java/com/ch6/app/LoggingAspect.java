4f3409fe5b13ff4c3332c6928873662192df9fe8
SerenaPang4 <ypang4@usfca.edu>
2024-10-15 16:33:08 -0700

    defining aspect class

---
+++ ch6/ch6-app/src/main/java/com/ch6/app/LoggingAspect.java
@@ -0,0 +1,9 @@
+package com.ch6.app;
+
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+	public void log() {}
+	
+}
--- ch6/ch6-app/src/main/java/com/ch6/app/ProjectConfig.java
+++ ch6/ch6-app/src/main/java/com/ch6/app/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.ch6.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -9,4 +10,8 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAspectJAutoProxy
 public class ProjectConfig {
 	
+	@Bean 
+	public LoggingAspect apect() {
+		return new LoggingAspect();
+	}
 }
