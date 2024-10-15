9e161cc8ebe7ce8082221be974ea4afe1d03131b
SerenaPang4 <ypang4@usfca.edu>
2024-10-15 14:51:16 -0700

    chapter 6 created

---
+++ ch6/ch6-app/src/main/java/com/ch6/app/App.java
@@ -0,0 +1,13 @@
+package com.ch6.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
