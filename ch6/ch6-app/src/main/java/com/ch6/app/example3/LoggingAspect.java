12e77c24ec3075ab452d6c0939060d8b6706a2bf
SerenaPang4 <ypang4@usfca.edu>
2024-10-16 15:18:53 -0700

    alter the parameters and returned value

---
+++ ch6/ch6-app/src/main/java/com/ch6/app/example3/LoggingAspect.java
@@ -0,0 +1,31 @@
+package com.ch6.app.example3;
+
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+	private Logger logger = Logger.getLogger(LoggingAspect.class.getName());
+	
+	@Around("execution(* com.ch6.app.example1.service.*.*(..))")
+	public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+		String MethodName = joinPoint.getSignature().getName();
+		Object[] arguments = joinPoint.getArgs();
+		
+		logger.info("Method " + MethodName + " with parameters " + Arrays.asList(arguments) 
+		 + " will execute");
+		Comment comment = new Comment();
+		comment.setText("Some other text!");
+		Object[] newArguments = {comment};
+		
+		Object returnedByMethod = joinPoint.proceed(newArguments);
+		
+		logger.info("Method executed and returned " + returnedByMethod);
+		
+		return returnedByMethod;
+	}
+}
---
+++ ch6/ch6-app/src/main/java/com/ch6/app/example3/ProjectConfig.java
@@ -0,0 +1,17 @@
+package com.ch6.app.example3;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@ComponentScan(basePackages = {"com.ch6.app.example2"})
+@EnableAspectJAutoProxy
+public class ProjectConfig {
+	
+	@Bean 
+	public LoggingAspect aspect() {
+		return new LoggingAspect();
+	}
+}
