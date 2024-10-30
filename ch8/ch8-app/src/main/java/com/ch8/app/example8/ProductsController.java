e216983f5d757fc86b60825a38cf88a1c90843cf
SerenaPang4 <ypang4@usfca.edu>
2024-10-30 14:07:57 -0700

    implementing the action method for adding a product

--- ch8/ch8-app/src/main/java/com/ch8/app/example8/ProductsController.java
+++ ch8/ch8-app/src/main/java/com/ch8/app/example8/ProductsController.java
@@ -1,10 +1,8 @@
-package com.ch8.app.example7;
+package com.ch8.app.example8;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ProductsController {
@@ -16,18 +14,6 @@ public class ProductsController {
 	
 	@RequestMapping("/products")
 	public String viewProducts(Model model) {
-		var products = productService.findAll();
-		model.addAttribute("products", products);
-		return "products.html";
-	}
-	
-	@RequestMapping(path = "/products", method = RequestMethod.POST)
-	public String addProduct(@RequestParam String name, @RequestParam double price, Model model) {
-		Product p = new Product();
-		p.setName(name);
-		p.setPrice(price);
-		productService.addProduct(p);
-		
 		var products = productService.findAll();
 		model.addAttribute("products", products);
 		return "products.html";
