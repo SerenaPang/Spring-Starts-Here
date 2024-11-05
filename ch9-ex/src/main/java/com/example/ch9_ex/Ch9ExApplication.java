eb262cc1ec13e3d12f7f0f267de923ab2e6b8c5b
SerenaPang4 <ypang4@usfca.edu>
2024-11-04 17:09:03 -0800

    update pom xml, add login.html and read me

---
+++ ch9-ex/src/main/java/com/example/ch9_ex/Ch9ExApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch9_ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch9ExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch9ExApplication.class, args);
+	}
+
+}
---
+++ ch9-ex/src/test/java/com/example/ch9_ex/Ch9ExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch9_ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch9ExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
